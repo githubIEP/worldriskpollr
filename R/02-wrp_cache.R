10d4b48f4195a8c4ea74af09705ce14870014767
David Hammond <dhammond@economicsandpeace.org>
2023-02-21 08:57:24 +1100

    simple download

--- R/02-wrp_cache.R
+++ R/02-wrp_cache.R
@@ -4,16 +4,8 @@
 #' @importFrom utils download.file unzip
 .wrp_cache <- function() {
   message("Downloading the World Risk Poll Data...\n")
-  filename <- c("wrp.zip")
-  url <- c("https://wrp.lrfoundation.org.uk/lrf_wrp_2021_full_data.zip")
+  filename <- "sysdata.rda"
+  url <- c("https://github.com/githubIEP/worldriskpollr/raw/master/data-raw/sysdata.rda")
   download.file(url, file.path(.wrp_package_folder(), filename), mode = "wb")
-  unzip(file.path(.wrp_package_folder(), filename),
-    exdir = file.path(.wrp_package_folder())
-  )
-  message("Processing the World Risk Poll...")
-  .wrp_processing()
-  files <- list.files(.wrp_package_folder(), full.names = TRUE)
-  files <- files[!grepl(basename(.wrp_sysdata_file_path()), files)]
-  file.remove(files)
   message("World Risk Poll is ready to use...")
 }
