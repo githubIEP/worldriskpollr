6bf9e1f65734fe54e3281314158f98357ffe3647
David Hammond <dhammond@economicsandpeace.org>
2023-02-23 00:03:29 +1100

    minor tweaks to output message aesthetics

--- R/zzz.R
+++ R/zzz.R
@@ -3,18 +3,18 @@
 #' @importFrom curl has_internet
 
 .onLoad <- function(lib, pkg) {
-  options(timeout = 120)
+  options(timeout = 2*60)
   test <- has_internet()
-  stopifnot("No internet connection detected. You need an active internet connection to use `worldriskpollr`.\nOnce connected, reload the package with `library(worldriskpollr)`" =
+  stopifnot("\nNo internet connection detected.\n...You need an active internet connection to use `worldriskpollr`.\n...Once connected, reload the package with `library(worldriskpollr)`" =
               test)
   url <- "https://github.com/githubIEP/worldriskpollr/raw/master/data-raw/sysdata.rda"
   url_exists <- HEAD(url)
-  stopifnot("World Risk Poll file not found. Please contact the package maintainer." =
+  stopifnot("\nWorld Risk Poll file not found.\n...Please contact the package maintainer." =
               (url_exists$status_code == 200))
   temp <- tempfile()
   dl <- suppressWarnings(try(download.file(url, temp, mode = "wb",
                                            quiet = TRUE), silent = TRUE))
-  stopifnot("World Risk Poll download timed-out.\nPlease check your internet connection and reload the package with `library(worldriskpollr)`" =
+  stopifnot("\nWorld Risk Poll download timed-out.\nPlease check your internet connection and reload the package with\n`library(worldriskpollr)`" =
               class(dl) != "try-error")
   .pkgenv$wrp <- readRDS(temp)
   unlink(temp)
