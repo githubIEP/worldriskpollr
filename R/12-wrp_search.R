90077ffc71d1a4887c3f8b3816b1f5ae33d8f2af
David Hammond <dhammond@economicsandpeace.org>
2023-02-22 13:25:35 +1100

    .onLoad added

--- R/12-wrp_search.R
+++ R/12-wrp_search.R
@@ -13,7 +13,7 @@
 #'
 
 wrp_search <- function(string = "violence") {
-  wrp <- .wrp_check()
+  wrp <- .pkgenv$wrp #.wrp_check()
   dict <- wrp$wrp_questions %>% unnest(.data$levels)
   matches <- unique(c(
     grep(string, dict$label, ignore.case = TRUE),
--- tests/testthat/test-03-wrp_remove.R
+++
@@ -1,5 +0,0 @@
-test_that("test_removal", {
-  wrp_remove()
-  expected <- wrp_get(geography = "country", wrp_question_uid = "Q1")
-  expect_s3_class(expected, "data.frame")
-})
